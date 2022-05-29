0f7d03fa78fffe284528a18da8d6b9965bec3f5c
BetterRage <benni.schmidties@gmail.com>
2022-05-29 13:01:17 +0200

    setup cmake and vs code

---
+++ build/CMakeFiles/WS2812B.dir/B_/Git-repos/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles\WS2812B.dir\B_\Git-repos\pico-sdk\src\rp2_common\hardware_xosc\xosc.c.obj: \
+ B:\Git-repos\pico-sdk\src\rp2_common\hardware_xosc\xosc.c \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/types.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ b:\programme\armgcc\arm-none-eabi\include\assert.h \
+ b:\programme\armgcc\arm-none-eabi\include\_ansi.h \
+ b:\programme\armgcc\arm-none-eabi\include\newlib.h \
+ b:\programme\armgcc\arm-none-eabi\include\_newlib_version.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\config.h \
+ b:\programme\armgcc\arm-none-eabi\include\machine\ieeefp.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\features.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ b:\programme\armgcc\arm-none-eabi\include\stdint.h \
+ b:\programme\armgcc\arm-none-eabi\include\machine\_default_types.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\_intsup.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\_stdint.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ B:\Git-repos\pico-dev\WS2812B\build\generated\pico_base/pico/version.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/config.h \
+ B:\Git-repos\pico-dev\WS2812B\build\generated\pico_base/pico/config_autogen.h \
+ B:/Git-repos/pico-sdk/src/boards/include/boards/pico.h \
+ B:/Git-repos/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\cdefs.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/types.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/error.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\hardware_clocks\include/hardware/clocks.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/clocks.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\hardware_base\include/hardware/address_mapped.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/addressmap.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/clocks.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/xosc.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\hardware_xosc\include/hardware/xosc.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/xosc.h
---
+++ build/CMakeFiles/WS2812B.dir/B_/Git-repos/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d
@@ -0,0 +1,30 @@
+CMakeFiles\WS2812B.dir\B_\Git-repos\pico-sdk\src\rp2_common\pico_float\float_math.c.obj: \
+ B:\Git-repos\pico-sdk\src\rp2_common\pico_float\float_math.c \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/types.h \
+ B:\Git-repos\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ b:\programme\armgcc\arm-none-eabi\include\assert.h \
+ b:\programme\armgcc\arm-none-eabi\include\_ansi.h \
+ b:\programme\armgcc\arm-none-eabi\include\newlib.h \
+ b:\programme\armgcc\arm-none-eabi\include\_newlib_version.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\config.h \
+ b:\programme\armgcc\arm-none-eabi\include\machine\ieeefp.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\features.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ b:\programme\armgcc\arm-none-eabi\include\stdint.h \
+ b:\programme\armgcc\arm-none-eabi\include\machine\_default_types.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\_intsup.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\_stdint.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\pico_float\include/pico/float.h \
+ b:\programme\armgcc\arm-none-eabi\include\math.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\reent.h \
+ b:\programme\armgcc\arm-none-eabi\include\_ansi.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\_types.h \
+ b:\programme\armgcc\arm-none-eabi\include\machine\_types.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\lock.h \
+ b:\programme\armgcc\arm-none-eabi\include\sys\cdefs.h \
+ b:\programme\armgcc\lib\gcc\arm-none-eabi\10.3.1\include\float.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\pico_bootrom\include/pico/bootrom/sf_table.h \
+ B:\Git-repos\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ B:\Git-repos\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h
