797322d022fec215babdb95174deb8a8a72a32ac
BetterRage <benni.schmidties@gmail.com>
2022-06-01 18:46:04 +0200

    abstraction and modular

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 #include <stdio.h>
-int main() {
+#include "WS2812B.h"
+
+uint32_t colours[8]={
+    WS2812B_24bit(255,0,0),
+    WS2812B_24bit(255,0,0),
+    WS2812B_24bit(0,0,255),
+    WS2812B_24bit(0,0,255),
+    WS2812B_24bit(0,255,0),
+    WS2812B_24bit(0,255,0),
+    WS2812B_24bit(255,0,0),
+    WS2812B_24bit(255,0,0)};
+
+int main() 
+{
+
     stdio_init_all();
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        printf("asd\n");
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+    gpio_init(WS2812B_pin);
+    gpio_set_dir(WS2812B_pin, GPIO_OUT);
+    set_sys_clock_khz(125000,true);
+    sleep_ms(200);
+    while(true)
+    {
+        WS2812B_reset();
+        WS2812B_sendArray(colours,8);
+        WS2812B_reset();
+        sleep_ms(2500);   
+        WS2812B_fillColour(WS2812B_24bit(0,0,0));
+        WS2812B_reset();
+        sleep_ms(300);      
     }
+    
 }
+
+
